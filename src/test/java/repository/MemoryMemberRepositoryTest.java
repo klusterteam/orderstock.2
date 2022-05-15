b1f5e6d14d5c5e4a7a5d169bee61008b1311814d
jaeh2020 <73710323+jaeh2020@users.noreply.github.com>
2022-05-15 23:22:54 +0900

    수정중

---
+++ src/test/java/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package repository;
+
+import com.kluster.orderstock_2.main.domain.Member;
+import com.kluster.orderstock_2.main.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    /* 초기화 */
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member(); // 전체 바꾸기 복사후 부분 클릭후 shift + f6
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.finaAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
