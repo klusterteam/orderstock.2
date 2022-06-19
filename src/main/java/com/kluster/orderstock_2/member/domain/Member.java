5f17f51c29bc296ba4723e77bed8d2d5d07b869d
jaeh2020 <73710323+jaeh2020@users.noreply.github.com>
2022-06-19 21:32:25 +0900

    H2-8.01 : 회원 - 로직정리, 테스트(입력확인)

---
+++ src/main/java/com/kluster/orderstock_2/member/domain/Member.java
@@ -0,0 +1,85 @@
+package com.kluster.orderstock_2.member.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String member_name;
+    private String member_id;
+    private String member_password;
+    private String member_email;
+    private int member_phone;
+    private String member_address;
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getMember_name() {
+        return member_name;
+    }
+
+    public void setMember_name(String member_name) {
+        this.member_name = member_name;
+    }
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getMember_password() {
+        return member_password;
+    }
+
+    public void setMember_password(String member_password) {
+        this.member_password = member_password;
+    }
+
+    public String getMember_email() {
+        return member_email;
+    }
+
+    public void setMember_email(String member_email) {
+        this.member_email = member_email;
+    }
+
+    public int getMember_phone() {
+        return member_phone;
+    }
+
+    public void setMember_phone(int member_phone) {
+        this.member_phone = member_phone;
+    }
+
+    public String getMember_address() {
+        return member_address;
+    }
+
+    public void setMember_address(String member_address) {
+        this.member_address = member_address;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "idx=" + idx +
+                ", member_name='" + member_name + '\'' +
+                ", member_id='" + member_id + '\'' +
+                ", member_password='" + member_password + '\'' +
+                ", member_email='" + member_email + '\'' +
+                ", member_phone='" + member_phone + '\'' +
+                ", member_address='" + member_address + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/kluster/orderstock_2/member/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,71 @@
+package com.kluster.orderstock_2.member.repository;
+
+import com.kluster.orderstock_2.member.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("os_member").usingGeneratedKeyColumns("idx");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getMember_name());
+        parameters.put("id", member.getMember_id());
+        parameters.put("password", member.getMember_password());
+        parameters.put("address", member.getMember_address());
+        parameters.put("email", member.getMember_email());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setIdx(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findByIdx(Long idx) {
+        List<Member> result = jdbcTemplate.query("select * from os_member where idx = ?", memberRowMapper(), idx);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findById(String id) {
+        List<Member> result = jdbcTemplate.query("select * from os_member where member_id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from os_member where member_name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from os_member", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper(){
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setIdx(rs.getLong("idx"));
+            member.setMember_name(rs.getString("member_name"));
+            return member;
+        };
+    }
+}
