5742b2a9366d4aeb7b2b366adb50419cac0bb38a
xkxltm12 <xkxltm12@naver.com>
2022-06-26 19:52:12 +0900

    H2-9.01: 나이스어드민 적용, 어드민 회원 리스트 회원구분 및 상세보기 페이지 구현중, Amember 컨트롤러 서비스 매퍼 도메인 추가

---
+++ src/main/java/com/kluster/orderstock_2/member/mapper/AdminMemberMapper.java
@@ -0,0 +1,13 @@
+package com.kluster.orderstock_2.member.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kluster.orderstock_2.member.domain.Amember;
+import com.kluster.orderstock_2.member.domain.Member;
+
+@Mapper
+public interface AdminMemberMapper {
+	List<Amember> getMemberList();
+}
