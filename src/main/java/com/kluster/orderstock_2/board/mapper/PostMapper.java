a4d69ac0313224a908f38e7f8218c838a1328a9c
ksk1908 <85006448+ksk1908@users.noreply.github.com>
2022-08-14 17:29:44 +0900

    S2-14.01: 게시판 관련 java 컨트롤러/서비스/매퍼 추가 + 공통 css 적용+ board 레이아웃 제거

--- src/main/java/com/kluster/orderstock_2/board/mapper/PostMapper.java
+++ src/main/java/com/kluster/orderstock_2/board/mapper/PostMapper.java
@@ -1,8 +1,14 @@
 package com.kluster.orderstock_2.board.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kluster.orderstock_2.board.domain.Post;
+
 @Mapper
-public class PostMapper {
+public interface PostMapper {
+
+	List<Post> getPostList(int boardCode);
 
 }
